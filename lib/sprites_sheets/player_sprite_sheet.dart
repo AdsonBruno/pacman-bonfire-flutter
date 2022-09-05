fb863c120b8e869594e8b63a9986820df4a8d82f
AdsonBruno <adsonbruno2@gmail.com>
2022-09-05 14:58:10 -0300

    mod: adicionando variável para armazenar o caminho da imagem do player e configurações de colisões

--- lib/sprites_sheets/player_sprite_sheet.dart
+++ lib/sprites_sheets/player_sprite_sheet.dart
@@ -1,79 +1,89 @@
 import 'package:bonfire/bonfire.dart';
 
 class PlayerSpriteSheet {
+  static String pathImagePlayer = 'pacman_player.png';
+
   static Future<SpriteAnimation> get pacmanIdLeft => SpriteAnimation.load(
-        'pacman_player.png',
+        // 'pacman_player.png',
+        pathImagePlayer,
         SpriteAnimationData.sequenced(
           amount: 1,
-          stepTime: 0.15,
+          stepTime: 0.09,
           textureSize: Vector2(24, 24),
           texturePosition: Vector2(72, 0),
         ),
       );
 
   static Future<SpriteAnimation> get pacmanIdRight => SpriteAnimation.load(
-        'pacman_player.png',
+        // 'pacman_player.png',
+        pathImagePlayer,
         SpriteAnimationData.sequenced(
           amount: 1,
-          stepTime: 0.15,
+          stepTime: 0.09,
           textureSize: Vector2(24, 24),
           texturePosition: Vector2(0, 0),
         ),
       );
 
   static Future<SpriteAnimation> get pacmanIdUp => SpriteAnimation.load(
-        'pacman_player.png',
+        // 'pacman_player.png',
+        pathImagePlayer,
         SpriteAnimationData.sequenced(
           amount: 1,
-          stepTime: 0.15,
+          stepTime: 0.09,
           textureSize: Vector2(24, 24),
           texturePosition: Vector2(0, 24),
         ),
       );
 
   static Future<SpriteAnimation> get pacmanIdDown => SpriteAnimation.load(
-        'pacman_player.png',
+        // 'pacman_player.png',
+        pathImagePlayer,
         SpriteAnimationData.sequenced(
           amount: 1,
-          stepTime: 0.15,
+          stepTime: 0.09,
           textureSize: Vector2(24, 24),
           texturePosition: Vector2(72, 24),
         ),
       );
 
   static Future<SpriteAnimation> get pacmanRunLeft => SpriteAnimation.load(
-        'pacman_player.png',
+        // 'pacman_player.png',
+        pathImagePlayer,
         SpriteAnimationData.sequenced(
           amount: 3,
-          stepTime: 0.15,
+          stepTime: 0.09,
           textureSize: Vector2(24, 24),
           texturePosition: Vector2(72, 0),
         ),
       );
 
   static Future<SpriteAnimation> get pacmanRunRight => SpriteAnimation.load(
-        'pacman_player.png',
+        // 'pacman_player.png',
+        pathImagePlayer,
         SpriteAnimationData.sequenced(
           amount: 3,
-          stepTime: 0.15,
+          stepTime: 0.09,
           textureSize: Vector2(24, 24),
           texturePosition: Vector2(0, 0),
         ),
       );
   static Future<SpriteAnimation> get pacmanRunUp => SpriteAnimation.load(
-      'pacman_player.png',
+      // 'pacman_player.png',
+      pathImagePlayer,
       SpriteAnimationData.sequenced(
         amount: 3,
-        stepTime: 0.15,
+        stepTime: 0.09,
         textureSize: Vector2(24, 24),
         texturePosition: Vector2(0, 24),
       ));
 
   static Future<SpriteAnimation> get pacmanRunDown => SpriteAnimation.load(
-        'pacman_player.png',
+        // 'pacman_player.png',
+        pathImagePlayer,
         SpriteAnimationData.sequenced(
           amount: 3,
-          stepTime: 0.10,
+          stepTime: 0.09,
           textureSize: Vector2(24, 24),
           texturePosition: Vector2(72, 24),
         ),
